d308f23be1d98d8aa22c0dbf696e420c49fc365c
Carlos Ivan Romero Hernandez <43647783+ivanrmrhdz@users.noreply.github.com>
2019-03-14 11:21:37 -0600

    Add files via upload

---
+++ classsss.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class paciente{     
+        
+    private:
+        
+            
+            
+            int corr;
+            char* nombre[20];
+            char* apellido[20];
+            int edad;
+            char* sexo;
+            double peso;
+            char* enfermedad[30];
+                  
+            
+            public:
+                paciente();
+                void ingresar();
+                void mostrar();
+      
+    
+    };
+
+void pacinete::ingresar(){
+    
+    
+    
+}
+
+void pacinete::mostrar(){
+    
+    
+}
+main(){
+    paciente *p1;
+    paciente *p2;
+    
+    
+ 
+  return 0;  
+}
